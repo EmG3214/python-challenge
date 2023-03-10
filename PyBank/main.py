9ddedd5b3c983a5767fcca96831931954f9e9489
Em Greene <EmGreene3214@gmail.com>
2023-03-09 22:17:06 -0500

    adding lines to print to txt file

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,5 +19,20 @@ with open(csvpath) as csvfile:
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
 
-#need to append data from the csv file into the empty lists
+    #need to append data from the csv file into the empty lists
+    for row in csvreader:
+        date_list.append(row[0])
+        profit_loss_list.append(int(row[1]))
+
+#check to make sure that worked
+print(f"Total Months {len(date_list)}")
+print(f"Total: ${sum(profit_loss_list)}")
+
+#in order to find the avg change, increases, and decreases, 
+#we have to calculate the changes between each month
+output_path = r"C:\Users\EmGre\OneDrive\Desktop\python-challenge\python-challenge\PyBank\analysis\pybank_analysis.txt"
+with open(output_path, "w") as txtfile:
+    txtfile.writelines(f"Total Months {len(date_list)} \nTotal: ${sum(profit_loss_list)} \nAverage Change:  \nGreatest Increase in Profits: \nGreatest Decrease in Profits: ")
+
+
 
